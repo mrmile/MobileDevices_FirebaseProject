feca829d9c3eea131162b8b7a536605e72935af9
mrmile <alvaro.milla02@gmail.com>
2022-12-13 00:35:14 +0100

    Made painting widget
    Tiene un pequeño bug al dibujar que se tendría que solucionar haciendo un add(null) en vez de add(null!) pero lo primero da muchos errores.

--- mobile_devices_firebase_tasks/lib/main.dart
+++ mobile_devices_firebase_tasks/lib/main.dart
@@ -1,115 +1,144 @@
+//import 'dart:ffi'; //Gives some errors - because of repeated declarations?
+import 'dart:ui';
 import 'package:flutter/material.dart';
 
-void main() {
+void main()
+{
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget
+{
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  Widget build(BuildContext context)
+  {
+    return MaterialApp
+    (
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+      theme: ThemeData
+      (
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'PaintScreen'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatefulWidget
+{
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _MyHomePageState extends State<MyHomePage>
+{
+  List<Offset> points = [];
 
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+  Widget build(BuildContext context)
+  {
+    final double referenceWidth = MediaQuery.of(context).size.width;
+    final double referenceHeight = MediaQuery.of(context).size.height;
+
+    return Scaffold
+    (
+      appBar: AppBar
+      (
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
+      body: Center
+      (
+        child: Column
+        (
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+          children: <Widget>
+          [
+            Container
+            (
+              width: referenceWidth * 0.80,
+              height: referenceHeight * 0.80,
+              color: Colors.white,
+
+              child: GestureDetector
+              (
+                onPanDown: (details)
+                {
+                  setState //this.setState...
+                  (() {
+                    points.add(details.localPosition);
+                  });
+                },
+                onPanUpdate: (details)
+                {
+                  setState
+                  (() {
+                    points.add(details.localPosition);
+                  });
+                },
+                onPanEnd: (details)
+                {
+                  setState
+                  (() {
+                    points.add(null!);
+                  });
+                },
+                child: CustomPaint
+                (
+                  painter: CustomPainterWidged(pointsPass: points),
+                ),
+              ),
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+class CustomPainterWidged extends CustomPainter
+{
+  List<Offset> pointsPass;
+
+  CustomPainterWidged({required this.pointsPass});
+
+  @override
+  void paint(Canvas canvas, Size size)
+  {
+    Paint drawingCanvas = Paint();
+    drawingCanvas.color = Colors.white;
+
+    Rect canvasRectangle = Rect.fromLTWH(0, 0, size.width, size.height);
+
+    canvas.drawRect(canvasRectangle, drawingCanvas);
+
+    Paint paint = Paint();
+    paint.color = Colors.black;
+    paint.strokeWidth = 2.0;
+    paint.strokeCap = StrokeCap.round;
+
+    for(int x = 0; x < pointsPass.length - 1; x++)
+    {
+      if(pointsPass[x] != null && pointsPass[x + 1] != null)
+      {
+        canvas.drawLine(pointsPass[x], pointsPass[x + 1], paint);
+      }
+      else if(pointsPass[x] != null && pointsPass[x + 1] == null)
+      {
+        canvas.drawPoints(PointMode.points, [pointsPass[x]], paint);
+      }
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate)
+  {
+    // TODO: implement shouldRepaint
+    return true;
+  }
+  
 }
