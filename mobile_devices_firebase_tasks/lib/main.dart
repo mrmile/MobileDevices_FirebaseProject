d1e0248946f10e940804bac66cf059da332ee735
mrmile <alvaro.milla02@gmail.com>
2022-12-15 00:55:33 +0100

    Finished drawing screen prototype

--- mobile_devices_firebase_tasks/lib/main.dart
+++ mobile_devices_firebase_tasks/lib/main.dart
@@ -17,6 +17,7 @@ class MyApp extends StatelessWidget
     return MaterialApp
     (
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData
       (
         primarySwatch: Colors.blue,
@@ -35,9 +36,26 @@ class MyHomePage extends StatefulWidget
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+class DrawingBrush
+{
+  Offset point = const Offset(0,0);
+  Paint brushPaint = Paint();
+
+  DrawingBrush(this.point, this.brushPaint);
+}
+
 class _MyHomePageState extends State<MyHomePage>
 {
-  List<Offset> points = [];
+  List<DrawingBrush> points = [];
+  Color selectedColor = Colors.red;
+  int currentSelectedColor = 1;
+  //DrawingBrush currentBrush = DrawingBrush({0,0}, Paint() ..color = Colors.black ..strokeCap = StrokeCap.round ..strokeWidth = 2.0);
+  //@override
+  //void initState()
+  //{
+  //  super.initState();
+  //  Color selectedColor = Colors.black;
+  //}
 
   @override
   Widget build(BuildContext context)
@@ -47,37 +65,34 @@ class _MyHomePageState extends State<MyHomePage>
 
     return Scaffold
     (
-      appBar: AppBar
-      (
-        title: Text(widget.title),
-      ),
       body: Center
       (
-        child: Column
+        child: Stack
         (
-          mainAxisAlignment: MainAxisAlignment.center,
+          //mainAxisAlignment: MainAxisAlignment.center,
+
           children: <Widget>
           [
             Container
             (
-              width: referenceWidth * 0.80,
-              height: referenceHeight * 0.80,
-              color: Colors.white,
-
+              width: referenceWidth * 0.99,
+              height: referenceHeight * 0.99,
+              color: const Color.fromARGB(255, 255, 254, 223),
+              
               child: GestureDetector
               (
                 onPanDown: (details)
                 {
                   setState //this.setState...
                   (() {
-                    points.add(details.localPosition);
+                    points.add(DrawingBrush(details.localPosition, Paint() ..color = selectedColor ..strokeCap = StrokeCap.round ..strokeWidth = 2.0));
                   });
                 },
                 onPanUpdate: (details)
                 {
                   setState
                   (() {
-                    points.add(details.localPosition);
+                    points.add(DrawingBrush(details.localPosition, Paint() ..color = selectedColor ..strokeCap = StrokeCap.round ..strokeWidth = 2.0));
                   });
                 },
                 onPanEnd: (details)
@@ -92,6 +107,393 @@ class _MyHomePageState extends State<MyHomePage>
                   painter: CustomPainterWidged(pointsPass: points),
                 ),
               ),
+            ),
+            Row
+            (
+              crossAxisAlignment: CrossAxisAlignment.end,
+              mainAxisAlignment: MainAxisAlignment.end,
+
+              children: <Widget>
+              [
+                Column
+                (
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  mainAxisAlignment: MainAxisAlignment.start,
+
+                  children: <Widget>
+                  [
+                    const SizedBox(height: 100),
+                    Container
+                    (
+                      height: 35.0,
+                      width: 35.0,
+                      color: Colors.transparent,
+
+                      child: FittedBox
+                      (
+                        child: FloatingActionButton
+                        (
+                          backgroundColor: Colors.red,
+                          onPressed: ()
+                          {
+                            currentSelectedColor = 1;
+                            setState //this.setState...
+                            (() {
+                              selectedColor = Colors.red;
+                            });
+                          },
+
+                          child: Container
+                          (
+                            decoration: BoxDecoration
+                            (
+                              color: Colors.transparent,
+                              borderRadius: const BorderRadius.all
+                              (
+                                Radius.circular(100),
+                              ),
+                              boxShadow:
+                              [
+                                if(currentSelectedColor == 1)
+                                (
+                                  BoxShadow
+                                  (
+                                    color: Colors.black.withOpacity(0.3),
+                                    spreadRadius: 7,
+                                    blurRadius: 7,
+                                    offset: const Offset(0, 0),
+                                  )
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 15),
+                    Container
+                    (
+                      height: 35.0,
+                      width: 35.0,
+                      color: Colors.transparent,
+                      child: FittedBox
+                      (
+                        child: FloatingActionButton
+                        (
+                          backgroundColor: Colors.blue,
+                          onPressed: ()
+                          {
+                            currentSelectedColor = 2;
+                            setState //this.setState...
+                            (() {
+                              selectedColor = Colors.blue;
+                            });
+                          },
+
+                          child: Container
+                          (
+                            decoration: BoxDecoration
+                            (
+                              color: Colors.transparent,
+                              borderRadius: const BorderRadius.all
+                              (
+                                Radius.circular(100),
+                              ),
+                              boxShadow:
+                              [
+                                if(currentSelectedColor == 2)
+                                (
+                                  BoxShadow
+                                  (
+                                    color: Colors.black.withOpacity(0.3),
+                                    spreadRadius: 7,
+                                    blurRadius: 7,
+                                    offset: const Offset(0, 0),
+                                  )
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 15),
+                    Container
+                    (
+                      height: 35.0,
+                      width: 35.0,
+                      color: Colors.transparent,
+                      child: FittedBox
+                      (
+                        child: FloatingActionButton
+                        (
+                          backgroundColor: Colors.orange,
+                          onPressed: ()
+                          {
+                            currentSelectedColor = 3;
+                            setState //this.setState...
+                            (() {
+                              selectedColor = Colors.orange;
+                            });
+                          },
+
+                          child: Container
+                          (
+                            decoration: BoxDecoration
+                            (
+                              color: Colors.transparent,
+                              borderRadius: const BorderRadius.all
+                              (
+                                Radius.circular(100),
+                              ),
+                              boxShadow:
+                              [
+                                if(currentSelectedColor == 3)
+                                (
+                                  BoxShadow
+                                  (
+                                    color: Colors.black.withOpacity(0.3),
+                                    spreadRadius: 7,
+                                    blurRadius: 7,
+                                    offset: const Offset(0, 0),
+                                  )
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 15),
+                    Container
+                    (
+                      height: 35.0,
+                      width: 35.0,
+                      color: Colors.transparent,
+                      child: FittedBox
+                      (
+                        child: FloatingActionButton
+                        (
+                          backgroundColor: Colors.green,
+                          onPressed: ()
+                          {
+                            currentSelectedColor = 4;
+                            setState //this.setState...
+                            (() {
+                              selectedColor = Colors.green;
+                            });
+                          },
+
+                          child: Container
+                          (
+                            decoration: BoxDecoration
+                            (
+                              color: Colors.transparent,
+                              borderRadius: const BorderRadius.all
+                              (
+                                Radius.circular(100),
+                              ),
+                              boxShadow:
+                              [
+                                if(currentSelectedColor == 4)
+                                (
+                                  BoxShadow
+                                  (
+                                    color: Colors.black.withOpacity(0.3),
+                                    spreadRadius: 7,
+                                    blurRadius: 7,
+                                    offset: const Offset(0, 0),
+                                  )
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 15),
+                    Container
+                    (
+                      height: 35.0,
+                      width: 35.0,
+                      color: Colors.transparent,
+                      child: FittedBox
+                      (
+                        child: FloatingActionButton
+                        (
+                          backgroundColor: Colors.yellow,
+                          onPressed: ()
+                          {
+                            currentSelectedColor = 5;
+                            setState //this.setState...
+                            (() {
+                              selectedColor = Colors.yellow;
+                            });
+                          },
+
+                          child: Container
+                          (
+                            decoration: BoxDecoration
+                            (
+                              color: Colors.transparent,
+                              borderRadius: const BorderRadius.all
+                              (
+                                Radius.circular(100),
+                              ),
+                              boxShadow:
+                              [
+                                if(currentSelectedColor == 5)
+                                (
+                                  BoxShadow
+                                  (
+                                    color: Colors.black.withOpacity(0.3),
+                                    spreadRadius: 7,
+                                    blurRadius: 7,
+                                    offset: const Offset(0, 0),
+                                  )
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 15),
+                    Container
+                    (
+                      height: 35.0,
+                      width: 35.0,
+                      color: Colors.transparent,
+                      child: FittedBox
+                      (
+                        child: FloatingActionButton
+                        (
+                          backgroundColor: Colors.black,
+                          onPressed: ()
+                          {
+                            currentSelectedColor = 6;
+                            setState //this.setState...
+                            (() {
+                              selectedColor = Colors.black;
+                            });
+                          },
+
+                          child: Container
+                          (
+                            decoration: BoxDecoration
+                            (
+                              color: Colors.transparent,
+                              borderRadius: const BorderRadius.all
+                              (
+                                Radius.circular(100),
+                              ),
+                              boxShadow:
+                              [
+                                if(currentSelectedColor == 6)
+                                (
+                                  BoxShadow
+                                  (
+                                    color: Colors.black.withOpacity(0.3),
+                                    spreadRadius: 7,
+                                    blurRadius: 7,
+                                    offset: const Offset(0, 0),
+                                  )
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 15),
+                    Container
+                    (
+                      height: 35.0,
+                      width: 35.0,
+                      color: Colors.transparent,
+                      child: FittedBox
+                      (
+                        child: FloatingActionButton
+                        (
+                          backgroundColor: Colors.white,
+                          onPressed: ()
+                          {
+                            currentSelectedColor = 7;
+                            setState //this.setState...
+                            (() {
+                              selectedColor = Colors.white;
+                            });
+                          },
+
+                          child: Container
+                          (
+                            decoration: BoxDecoration
+                            (
+                              color: Colors.transparent,
+                              borderRadius: const BorderRadius.all
+                              (
+                                Radius.circular(100),
+                              ),
+                              boxShadow:
+                              [
+                                if(currentSelectedColor == 7)
+                                (
+                                  BoxShadow
+                                  (
+                                    color: Colors.black.withOpacity(0.3),
+                                    spreadRadius: 7,
+                                    blurRadius: 7,
+                                    offset: const Offset(0, 0),
+                                  )
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 50),
+                    Container
+                    (
+                      height: 35.0,
+                      width: 35.0,
+                      color: Colors.transparent,
+                      child: FittedBox
+                      (
+                        child: FloatingActionButton
+                        (
+                          backgroundColor: Colors.white,
+                          child: const Icon(Icons.delete, color: Colors.grey),
+                          onPressed: ()
+                          {
+                            points.clear();
+                          }
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 15),
+                    Container
+                    (
+                      height: 35.0,
+                      width: 35.0,
+                      color: Colors.transparent,
+                      child: FittedBox
+                      (
+                        child: FloatingActionButton
+                        (
+                          backgroundColor: Colors.white,
+                          child: const Icon(Icons.chat_bubble, color: Colors.blue),
+                          onPressed: ()
+                          {
+                            // Will take you to the chat screen
+                          }
+                        ),
+                      ),
+                    ),
+                  ]
+                ),
+                const SizedBox(width: 25),
+              ],
             )
           ],
         ),
@@ -102,7 +504,7 @@ class _MyHomePageState extends State<MyHomePage>
 
 class CustomPainterWidged extends CustomPainter
 {
-  List<Offset> pointsPass;
+  List<DrawingBrush> pointsPass;
 
   CustomPainterWidged({required this.pointsPass});
 
@@ -110,26 +512,28 @@ class CustomPainterWidged extends CustomPainter
   void paint(Canvas canvas, Size size)
   {
     Paint drawingCanvas = Paint();
-    drawingCanvas.color = Colors.white;
+    drawingCanvas.color = const Color.fromARGB(255, 255, 254, 223);
 
     Rect canvasRectangle = Rect.fromLTWH(0, 0, size.width, size.height);
 
     canvas.drawRect(canvasRectangle, drawingCanvas);
 
-    Paint paint = Paint();
-    paint.color = Colors.black;
-    paint.strokeWidth = 2.0;
-    paint.strokeCap = StrokeCap.round;
+    //Paint paint = Paint();
+    //paint.color = Colors.black;
+    //paint.strokeWidth = 2.0;
+    //paint.strokeCap = StrokeCap.round;
 
     for(int x = 0; x < pointsPass.length - 1; x++)
     {
       if(pointsPass[x] != null && pointsPass[x + 1] != null)
       {
-        canvas.drawLine(pointsPass[x], pointsPass[x + 1], paint);
+        Paint paint = pointsPass[x].brushPaint;
+        canvas.drawLine(pointsPass[x].point, pointsPass[x + 1].point, paint);
       }
       else if(pointsPass[x] != null && pointsPass[x + 1] == null)
       {
-        canvas.drawPoints(PointMode.points, [pointsPass[x]], paint);
+        Paint paint = pointsPass[x].brushPaint;
+        canvas.drawPoints(PointMode.points, [pointsPass[x].point], paint);
       }
     }
   }
